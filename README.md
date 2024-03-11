fc4130a3b0240410d2f493b48681a34af6a7d9c2
Cesar Guerrero <cguerrero@Cesars-MBP.attlocal.net>
2024-03-10 23:16:58 -0700

    Added correct path and references to opencv library via CMake

---
+++ README.md
@@ -0,0 +1,7 @@
+# Open CV Learning
+This repository will be evidence of my OpenCV journey. I plan to take this learning and complete (3) projects.
+
+## Projects
+1. Morse-Code Translator using eye tracking
+2. Parking Monitor for my home
+3. Lane Assist using Raspberry PI in my car
---
+++ main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Cesar Guerrero
+ * 03/06/2024
+ * Getting started with Images - OpenCV
+ * 
+ * Learning Topics:
+ * - Reading an image
+ * - Checking image attribtues like datatype and shape
+ * - Matrix representation of an image
+ * - Color Images and splitting/merging image channels
+ * - Displaying images
+ * - Saving images
+*/
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <opencv2/imgcodecs.hpp>
+
+int main(){
+
+    return 0;
+}
+
